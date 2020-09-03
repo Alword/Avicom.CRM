1a745f342346430e3da5c95b21a70c6fc174a5ab
Alword <anzer987@yandex.ru>
2020-09-03 13:40:10 +0300

    feature сonverter based on the description

--- Avicom.CRM.Data/Enums/ContractStatus.cs
+++ Avicom.CRM.Data/Enums/ContractStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace Avicom.CRM.Data.Enums
 {
     public enum ContractStatus
     {
+        [Description("test1")]
         NotYetConcluded,
+        [Description("test2")]
         Concluded,
+        [Description("test3")]
         Cancelled
     }
 }
