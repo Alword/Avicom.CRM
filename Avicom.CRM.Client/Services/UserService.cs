84610e708a57533bd2e4107d4c88b769ad8afec6
Alword <anzer987@yandex.ru>
2020-09-04 09:31:21 +0300

    add user service

---
+++ Avicom.CRM.Client/Services/UserService.cs
@@ -0,0 +1,56 @@
+﻿using Avicom.CRM.Data;
+using DevExpress.Mvvm.UI;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avicom.CRM.Client.Services
+{
+    public class UserService
+    {
+        public User[] Users(Expression<Func<User, bool>> expression)
+        {
+            using (AvicomContext context = new AvicomContext())
+            {
+                return context.Users.Where(expression).AsNoTracking().ToArray();
+            }
+        }
+        public async Task<User> AddUserAsync(User user)
+        {
+            using (AvicomContext context = new AvicomContext())
+            {
+                context.Users.Add(user);
+                await context.SaveChangesAsync();
+                return user;
+            }
+        }
+        public async Task<int> RemoveUserAsync(Expression<Func<User, bool>> expression)
+        {
+            using (AvicomContext context = new AvicomContext())
+            {
+                var users = context.Users.Where(expression).AsNoTracking();
+                context.Users.RemoveRange(users);
+                return await context.SaveChangesAsync();
+            }
+        }
+        public async Task UpdateUserAsync(User user)
+        {
+            using (AvicomContext context = new AvicomContext())
+            {
+                var editUser = await context.Users.SingleOrDefaultAsync(u => u.Id == user.Id);
+                if (editUser != null)
+                {
+                    editUser.Login = user.Login;
+                    editUser.Name = user.Name;
+                    editUser.Password = user.Password;
+                }
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
+
